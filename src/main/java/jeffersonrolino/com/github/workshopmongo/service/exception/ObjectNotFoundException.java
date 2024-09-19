e804755bc66eb8c6db3191b08db5d53130e6551d
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-19 18:22:03 -0300

    Obtendo um usuário por id

---
+++ src/main/java/jeffersonrolino/com/github/workshopmongo/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package jeffersonrolino.com.github.workshopmongo.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+}
