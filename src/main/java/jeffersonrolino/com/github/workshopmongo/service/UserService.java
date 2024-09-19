27a8e8ac7376cff5460042d4241462814379ee23
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-19 06:28:11 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/jeffersonrolino/com/github/workshopmongo/service/UserService.java
@@ -0,0 +1,20 @@
+package jeffersonrolino.com.github.workshopmongo.service;
+
+import jeffersonrolino.com.github.workshopmongo.domain.User;
+import jeffersonrolino.com.github.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
