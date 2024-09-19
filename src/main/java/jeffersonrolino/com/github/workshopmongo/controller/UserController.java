69ca9d8d7455dd2d17935c7b5deba27a22a84829
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-19 19:09:41 -0300

    Deleção de usuário com DELETE

--- src/main/java/jeffersonrolino/com/github/workshopmongo/controller/UserController.java
+++ src/main/java/jeffersonrolino/com/github/workshopmongo/controller/UserController.java
@@ -39,4 +39,10 @@ public class UserController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
