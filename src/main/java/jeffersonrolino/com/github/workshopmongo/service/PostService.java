9d9bd1f79a822abb604ddf852eb71ea34f8adf21
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-20 02:54:01 -0300

    Obtendo um Post por Id

---
+++ src/main/java/jeffersonrolino/com/github/workshopmongo/service/PostService.java
@@ -0,0 +1,27 @@
+package jeffersonrolino.com.github.workshopmongo.service;
+
+import jeffersonrolino.com.github.workshopmongo.domain.Post;
+import jeffersonrolino.com.github.workshopmongo.domain.User;
+import jeffersonrolino.com.github.workshopmongo.repository.PostRepository;
+import jeffersonrolino.com.github.workshopmongo.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> post = postRepository.findById(id);
+        if(post.isPresent()){
+            return post.get();
+        }
+        else {
+            throw new ObjectNotFoundException("Post não encontrado");
+        }
+    }
+}
